06a9799270ea202ba7a51b07cdc5e7d07e8ec0c3
Sebastian J. Mielke <sjm@sjmielke.com>
2014-10-25 17:06:09 +0200

    Performance work. Also: likely different logic, but... pffff. Also: why is optimizing performance always so annoying and unrewarding :( I probably missed the most obvious problems and "optimized" some small stuff... eh, perhaps future me will be interested in continuing this work.

--- CorpusParsing.hs
+++ CorpusParsing.hs
@@ -1,56 +1,57 @@
-import Data.Char (isLetter)
-import Data.List (intersperse, sort, group, sortBy)
+import Data.Char (isLetter, isDigit)
+import Data.List (sortBy)
+import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
 import Data.Ord (comparing, Down(..))
 import Text.Printf
 import Text.XML.Light
 import Debug.Trace (trace)
 
+main :: IO ()
 main = do aCorpus <- fmap (cleanCorpusFiltering (\(Element n _ _ _) -> n == unqual "p"))
                    $ readFile "mueller_clean_orig_input.html"
           bCorpus <- fmap (cleanCorpusFiltering (\(Element _ as _ _) -> as == [Attr (unqual "align") "justify"]))
                    $ readFile "schmid_clean_orig_input.html"
-          let getProblems = concat
-                          . intersperse "\n"
-                          . filter (\s -> length s > 1 && (not $ isLetter (last s) && isLetter (head s)))
-                       
+          let getProblems = unlines . filter (\s -> not $ length s == 1 || all isLetter s || all isDigit s)
+
           putStrLn $ "A / mueller\n" ++ ppOccTable aCorpus
           putStrLn $ "B / schmid\n" ++ ppOccTable bCorpus
+          print $ length $ getProblems $ aCorpus ++ bCorpus
 
 cleanCorpusFiltering :: (Element -> Bool) -> String -> [String]
-cleanCorpusFiltering pred = cleanWordlistString
-                          . concatMap stringFromElement
-                          . filter pred
-                          . onlyElems
-                          . parseXML
-
-
-cleanWordlistString :: String -> [String]
-cleanWordlistString = spliceWords
-                    . concatMap separatePunctuation
-                    . map removeGuillemets
-                    . concatMap separatePunctuation
-                    . map (\x -> if x == "..." then "…" else x)
-                    . words
-    where separatePunctuation w = if length w > 1 && last w `elem` ['.', ',', '!', '?', ':', ';']
-                                  then [init w, [last w]]
-                                  else [w]
+cleanCorpusFiltering p = -- Removed guillemets may have uncovered one more layer of punctuation
+                         foldr (separatePunctuation id) []
+                       -- Separate punctuation and remove guillemets in the same pass
+                       . foldr (separatePunctuation removeGuillemets) []
+                       -- Splice linebreak-separated words
+                       . foldr (\w acc -> if length w > 1 && last w == '-'
+                                          then (init w ++ head acc) : tail acc
+                                          else w : acc) []
+                       . words
+                       . concatMap stringFromElement
+                       . filter p
+                       . onlyElems
+                       . parseXML
+    where separatePunctuation finfunc w acc
+            | w == "..." = "…" : acc
+            | otherwise = if length w > 1 && last w `elem` ['.', ',', '!', '?', ':', ';']
+                          then if length w > 3 && take 3 (reverse w) == "..."
+                               then finfunc (init . init . init $ w) : "…" : acc
+                               else finfunc (init w) : [last w] : acc
+                          else finfunc w : acc
           removeGuillemets w = let nopre  = if head w `elem` ['»', '„', '"']
                                             then (try tail) w else w
                                    nopost = if last nopre `elem` ['«', '“', '"']
                                             then (try init) nopre else nopre
-                                   try f w = if length w > 1 then f w else w
+                                   try f l = if length l > 1 then f l else l
                                in nopost
-          spliceWords [w] = [w]
-          spliceWords (w:wn:ws) = if last w == '-'
-                                  then spliceWords ((init w ++ wn) : ws)
-                                  else w : spliceWords (wn:ws)
 
 stringFromElement :: Element -> String
 stringFromElement = foldr go "" . elContent
     where go :: Content -> String -> String
           go (Elem el) acc = stringFromElement el ++ acc
           go (Text cd) acc = cdData cd ++ acc
+          go (CRef _) acc = acc
 
 ppOccTable :: [String] -> String
 ppOccTable corpus =  hr
@@ -67,20 +68,20 @@ ppOccTable corpus =  hr
                   ++ replicate (wwidth + nwidth - 7) ' ' ++ " wo./sen.|\n"
                   ++ hr
                   ++ "| . ! ?" ++ replicate (wwidth + nwidth + 2) ' ' ++ "|\n"
-                  ++ printf ("|%5.2f") ((fromJust $ lookup "." intable) `percentOf` sentenceEnds)
-                  ++ printf (" %5.2f") ((fromJust $ lookup "!" intable) `percentOf` sentenceEnds)
-                  ++ printf (" %5.2f") ((fromJust $ lookup "?" intable) `percentOf` sentenceEnds)
+                  ++ printf ("|%5.2f") ((fromJust $ M.lookup "." inmap) `percentOf` sentenceEnds)
+                  ++ printf (" %5.2f") ((fromJust $ M.lookup "!" inmap) `percentOf` sentenceEnds)
+                  ++ printf (" %5.2f") ((fromJust $ M.lookup "?" inmap) `percentOf` sentenceEnds)
                   ++ replicate (wwidth + nwidth - 9) ' ' ++ "|\n"
                   ++ hr
-    where intable = map (\n -> (head n, length n)) . group . sort $ corpus
+    where inmap = foldl (\m w -> M.insertWith (+) w 1 m) (M.empty :: M.Map String Int) corpus
           table = take 30
                 $ sortBy (comparing (Down . snd))
-                $ intable
+                $ M.toList inmap
           wwidth = maximum $ map (length . fst) table
           nwidth = maximum $ map (length . show . snd) table
           hr = "+" ++ replicate wwidth '-' ++ "+" ++ replicate nwidth '-' ++ "+------+\n"
-          allWords = sum $ map snd intable
+          allWords = sum $ M.elems inmap
           a `percentOf` b = 100 * fromIntegral a / fromIntegral b :: Double
-          sumWords ws = sum $ map snd $ filter (\(x,n) -> head x `elem` ws) intable
+          sumWords ws = sum $ M.elems $ M.filterWithKey (\x _ -> head x `elem` ws) inmap
           punctuation = sumWords ['.', '!', '?', ':', ';']
           sentenceEnds = sumWords ['.', '!', '?']
