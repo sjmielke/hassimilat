27718fd5db3bf1fcebe826f33b4d7c31d7cfd011
Sebastian J. Mielke <sjm@sjmielke.com>
2014-10-26 17:29:38 +0100

    New module generates bigrams. ...also, it can create new "similar" texts from them... this is fun :D

--- CorpusParsing.hs
+++ CorpusParsing.hs
@@ -1,3 +1,8 @@
+module CorpusParsing (
+    getCorpora,
+    ppOccTable
+    ) where
+
 import Data.Char (isLetter, isDigit)
 import Data.List (sortBy)
 import qualified Data.Map.Strict as M
@@ -7,16 +12,19 @@ import Text.Printf
 import Text.XML.Light
 import Debug.Trace (trace)
 
-main :: IO ()
-main = do aCorpus <- fmap (cleanCorpusFiltering (\(Element n _ _ _) -> n == unqual "p"))
-                   $ readFile "mueller_clean_orig_input.html"
-          bCorpus <- fmap (cleanCorpusFiltering (\(Element _ as _ _) -> as == [Attr (unqual "align") "justify"]))
-                   $ readFile "schmid_clean_orig_input.html"
-          let getProblems = unlines . filter (\s -> not $ length s == 1 || all isLetter s || all isDigit s)
+getCorpora :: IO ([String], [String])
+getCorpora = do aCorpus <- fmap (cleanCorpusFiltering (\(Element n _ _ _) -> n == unqual "p"))
+                         $ readFile "mueller_clean_orig_input.html"
+                bCorpus <- fmap (cleanCorpusFiltering (\(Element _ as _ _) -> as == [Attr (unqual "align") "justify"]))
+                         $ readFile "schmid_clean_orig_input.html"
+                let getProblems = unlines . filter (\s -> not $ length s == 1 || all isLetter s || all isDigit s)
 
-          putStrLn $ "A / mueller\n" ++ ppOccTable aCorpus
-          putStrLn $ "B / schmid\n" ++ ppOccTable bCorpus
-          print $ length $ getProblems $ aCorpus ++ bCorpus
+                -- putStrLn $ "A / mueller\n" ++ ppOccTable (countWords aCorpus)
+                -- putStrLn $ "B / schmid\n" ++ ppOccTable (countWords bCorpus)
+                putStrLn $ ("Problems in A: "++) $ show . length $ getProblems aCorpus
+                putStrLn $ ("Problems in B: "++) $ show . length $ getProblems bCorpus
+
+                return (aCorpus, bCorpus)
 
 cleanCorpusFiltering :: (Element -> Bool) -> String -> [String]
 cleanCorpusFiltering p = -- Removed guillemets may have uncovered one more layer of punctuation
@@ -45,36 +53,36 @@ cleanCorpusFiltering p = -- Removed guillemets may have uncovered one more layer
                                             then (try init) nopre else nopre
                                    try f l = if length l > 1 then f l else l
                                in nopost
+          stringFromElement = foldr go "" . elContent
+              where go :: Content -> String -> String
+                    go (Elem el) acc = stringFromElement el ++ acc
+                    go (Text cd) acc = cdData cd ++ acc
+                    go (CRef _) acc = acc
 
-stringFromElement :: Element -> String
-stringFromElement = foldr go "" . elContent
-    where go :: Content -> String -> String
-          go (Elem el) acc = stringFromElement el ++ acc
-          go (Text cd) acc = cdData cd ++ acc
-          go (CRef _) acc = acc
+countWords :: [String] -> M.Map String Int
+countWords = foldl (\m w -> M.insertWith (+) w 1 m) (M.empty :: M.Map String Int)
 
-ppOccTable :: [String] -> String
-ppOccTable corpus =  hr
-                  ++ printf ("|%" ++ show (wwidth + nwidth + 2) ++ "d")
-                            allWords
-                  ++ " words|\n"
-                  ++ hr
-                  ++ concatMap (\(w, n) -> printf ("|%" ++ show wwidth ++ "s") w
-                                        ++ printf ("|%" ++ show nwidth ++ "d") n
-                                        ++ printf ("|%6.4f|\n") (n `percentOf` allWords))
-                               table
-                  ++ hr
-                  ++ printf ("|%6.3f") (fromIntegral allWords / fromIntegral punctuation :: Double)
-                  ++ replicate (wwidth + nwidth - 7) ' ' ++ " wo./sen.|\n"
-                  ++ hr
-                  ++ "| . ! ?" ++ replicate (wwidth + nwidth + 2) ' ' ++ "|\n"
-                  ++ printf ("|%5.2f") ((fromJust $ M.lookup "." inmap) `percentOf` sentenceEnds)
-                  ++ printf (" %5.2f") ((fromJust $ M.lookup "!" inmap) `percentOf` sentenceEnds)
-                  ++ printf (" %5.2f") ((fromJust $ M.lookup "?" inmap) `percentOf` sentenceEnds)
-                  ++ replicate (wwidth + nwidth - 9) ' ' ++ "|\n"
-                  ++ hr
-    where inmap = foldl (\m w -> M.insertWith (+) w 1 m) (M.empty :: M.Map String Int) corpus
-          table = take 30
+ppOccTable :: M.Map String Int -> String
+ppOccTable inmap =  hr
+                 ++ printf ("|%" ++ show (wwidth + nwidth + 2) ++ "d")
+                           allWords
+                 ++ " words|\n"
+                 ++ hr
+                 ++ concatMap (\(w, n) -> printf ("|%" ++ show wwidth ++ "s") w
+                                       ++ printf ("|%" ++ show nwidth ++ "d") n
+                                       ++ printf ("|%6.4f|\n") (n `percentOf` allWords))
+                              table
+                 ++ hr
+                 ++ printf ("|%6.3f") (fromIntegral allWords / fromIntegral punctuation :: Double)
+                 ++ replicate (wwidth + nwidth - 7) ' ' ++ " wo./sen.|\n"
+                 ++ hr
+                 ++ "| . ! ?" ++ replicate (wwidth + nwidth + 2) ' ' ++ "|\n"
+                 ++ printf ("|%5.2f") ((fromJust $ M.lookup "." inmap) `percentOf` sentenceEnds)
+                 ++ printf (" %5.2f") ((fromJust $ M.lookup "!" inmap) `percentOf` sentenceEnds)
+                 ++ printf (" %5.2f") ((fromJust $ M.lookup "?" inmap) `percentOf` sentenceEnds)
+                 ++ replicate (wwidth + nwidth - 9) ' ' ++ "|\n"
+                 ++ hr
+    where table = take 30
                 $ sortBy (comparing (Down . snd))
                 $ M.toList inmap
           wwidth = maximum $ map (length . fst) table
---
+++ NGrams.hs
@@ -0,0 +1,44 @@
+-- module NGrams where
+
+import CorpusParsing
+
+import Control.Monad.State.Lazy (evalState, get, put)
+import Data.List (sortBy)
+import Data.Ord (comparing, Down(..))
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromJust)
+import System.IO.Unsafe (unsafePerformIO)
+import System.Random (randomR, getStdGen)
+
+type BigramMap = M.Map String (M.Map String Int)
+
+main :: IO ()
+main = do (aCorpus, bCorpus) <- getCorpora
+          let aBigrams = getBigrams aCorpus
+          let bBigrams = getBigrams bCorpus
+          
+          putStrLn "Sample A"
+          putStrLn $ unwords $ take 100 $ buildText aBigrams
+          putStrLn "Sample B"
+          putStrLn $ unwords $ take 100 $ buildText bBigrams
+
+-- getBigrams ignores the data for the first and last word, why bother,
+-- last one is likely just a dot or something similar anyway.
+getBigrams :: [String] -> BigramMap
+getBigrams = go M.empty
+    where go m (wp:w:ws) = go (M.insertWith (\_ -> M.insertWith (+) w 1) wp (M.singleton w 1) m) (w:ws)
+          go m _ = m
+
+buildText :: BigramMap -> [String]
+buildText bigrams = evalState (next ".") (unsafePerformIO getStdGen)
+    where next wp = do randomGen <- get
+                       let (n, newGen) = randomR (-3.0, 0.0 :: Double) randomGen
+                       put newGen
+                       let w = fst
+                             $ (\l ->  l !! (floor $ (fromIntegral $ min 10 (length l)) * exp n))
+                             $ sortBy (comparing (Down . snd))
+                             $ M.toList
+                             $ fromJust
+                             $ M.lookup wp bigrams
+                       following <- next w
+                       return (w : following)
