581fbc578015433bd59912a1c9a3a9c601a616d7
Sebastian J. Mielke <sjm@sjmielke.com>
2014-12-21 16:12:18 +0100

    Counting rules while reading off. Also, the previous commit wasn't even correct. But this one is. Or at least I hope so.

--- main.hs
+++ main.hs
@@ -5,6 +5,8 @@ import MyCorpusData
 import Control.DeepSeq (deepseq)
 import qualified Data.Foldable as F
 import qualified Data.Map.Strict as M
+import Data.Function (on)
+import Data.List (sortBy, nub, group)
 import Data.Maybe (fromJust)
 import qualified Data.Set as S
 import Data.Tree (Tree(Node), rootLabel, subForest, drawTree)
@@ -36,7 +38,7 @@ main = do {- aCorpus <- getWordListCorpus Mueller
           tiger <- getTreeCorpus Tiger
           -- putStrLn $ drawTree . fmap show $ tiger !! 42
           
-          let addIntoMap oldmap (T p w) = M.insertWith (\_ -> M.insertWith (+) w 1)
+          {- let addIntoMap oldmap (T p w) = M.insertWith (\_ -> M.insertWith (+) w 1)
                                                        p
                                                        (M.singleton w 1)
                                                        oldmap
@@ -44,14 +46,28 @@ main = do {- aCorpus <- getWordListCorpus Mueller
           let posChoices = F.foldl' (F.foldl' addIntoMap)
                                     (M.empty :: M.Map String (M.Map String Int))
                                     tiger
-          -- putStrLn $ ppOccTable $ fromJust $ M.lookup "ART" posChoices
+          putStrLn $ ppOccTable $ fromJust $ M.lookup "ART" posChoices
+          -- -}
           
           -- putStrLn $ unlines $ map show $ filter (fst . snd) $ M.toAscList $ getUsedTags tiger
           
-          let rules = getRules tiger
+          -- let rules = getRules tiger
           
-          print $ S.size rules
-          putStrLn $ unlines . map show $ filter (\(x,_) -> x == "ROOT") $ S.toList rules
+          -- print $ S.size rules
+          -- putStrLn $ unlines . map show $ filter (\(x,_) -> x == "ROOT") $ S.toList rules
+          
+          let rulesOcc = countRules tiger
+          
+          {- putStrLn $ unlines . map show
+                   $ sortBy (compare `on` snd)
+                   $ filter (\((x,_),_) -> x == "ROOT")
+                   $ M.toList rulesOcc
+          -- -}
+          
+          print $ M.size rulesOcc
+          let usedTags = map (\l -> (head l, length l)) . group $ map fst $ M.keys rulesOcc
+          putStrLn $ unlines . map show $ usedTags
+          
 
 getUsedTags :: [Tree SNode] -> M.Map String (Bool, Bool) -- name, used in inner node (NT), used in leaf (T)
 getUsedTags = F.foldl' (F.foldl' ins)
@@ -62,9 +78,17 @@ getUsedTags = F.foldl' (F.foldl' ins)
 type Rule = (String, [String])
 
 getRules :: [Tree SNode] -> S.Set Rule
-getRules = F.foldl' (flip readoff)
-                    (S.empty :: S.Set Rule)
-    where readoff (Node (NT rootTag) children) =
+getRules = F.foldl' readoff (S.empty :: S.Set Rule)
+    where readoff ruleset (Node (NT rootTag) children) =
                 let newrule = (rootTag, map (tag . rootLabel) children)
-                in newrule `deepseq` S.insert newrule
-          readoff _ = id
+                in newrule `deepseq` 
+                   F.foldl' readoff (S.insert newrule ruleset) children
+          readoff ruleset _ = ruleset
+
+countRules :: [Tree SNode] -> M.Map Rule Int
+countRules = F.foldl' readoff (M.empty :: M.Map Rule Int)
+    where readoff rulemap (Node (NT rootTag) children) =
+                let newrule = (rootTag, map (tag . rootLabel) children)
+                in newrule `deepseq`
+                   F.foldl' readoff (M.insertWith (+) newrule 1 rulemap) children
+          readoff rulemap _ = rulemap
